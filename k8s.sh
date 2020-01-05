3341694d4dc53b51cda7c6523c2512dd9df781b4
Stephen Wike <stephen.c.wise@gmail.com>
2020-01-04 23:37:36 -0700

    trying to fing a bug

--- k8s.sh
+++ k8s.sh
@@ -29,7 +29,7 @@ install_kubernetes() {
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
     cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
         deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF    
+    EOF    
     apt update
     apt install -y \
         kubelet \
