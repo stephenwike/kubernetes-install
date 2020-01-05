e8777472da45a230cf7332756f463f077ce6440f
Stephen Wike <stephen.c.wise@gmail.com>
2020-01-04 23:39:35 -0700

    trying something else

--- k8s.sh
+++ k8s.sh
@@ -27,9 +27,11 @@ install_kubernetes() {
         apt-transport-https \
         curl
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    
     cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
         deb https://apt.kubernetes.io/ kubernetes-xenial main
-    EOF    
+EOF
+
     apt update
     apt install -y \
         kubelet \
