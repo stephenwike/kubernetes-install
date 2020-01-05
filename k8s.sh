e1d2d946fe78b96cb1cf388392ee5ed2f12e65d9
Stephen Wike <stephen.c.wise@gmail.com>
2020-01-04 22:48:01 -0700

    added docker installation to script

--- k8s.sh
+++ k8s.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "HELLO WORLD!"
+install_docker() {
+    echo "Installing Docker"
+
+    sudo apt update
+    sudo apt install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+    sudo systemctl enable docker   
+}
+
+install_docker
