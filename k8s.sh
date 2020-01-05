a0778d9196aa6a2c85fe7dd849503bea010d1197
Stephen Wike <stephen.c.wise@gmail.com>
2020-01-04 23:06:23 -0700

    added more to docker install script

--- k8s.sh
+++ k8s.sh
@@ -4,8 +4,19 @@ install_docker() {
     echo "Installing Docker"
 
     sudo apt update
-    sudo apt install -y docker-ce
-    sudo systemctl enable docker   
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    # TODO: Validate fingerprint - https://docs.docker.com/v17.09/engine/installation/linux/docker-ce/ubuntu/#set-up-the-repository
+    sudo add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce
 }
 
 install_docker
