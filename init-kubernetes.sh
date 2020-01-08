23bc2f1927a9523200ed2844f0b969c874d1bdf6
Stephen Wike <stephen.wike@terumobct.com>
2020-01-08 16:13:51 -0700

    update init-kubernetes script

--- init-kubernetes.sh
+++ init-kubernetes.sh
@@ -7,17 +7,18 @@ if [ "$1" != "" ]; then
     echo "APIServerAddress: $apiserveraddr"
     kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$apiserveraddr
 
-    adduser $1 sudo --disabled-password
+    adduser $1 -gecos "$1,,," --disabled-password
+    usermod -aG sudo $1
 
     if [ "$2" != "" ]; then
         echo "$1:$2" | chpasswd
     fi
 
     mkdir -p /home/$1/.kube
-    cp -i /etc/kubernetes/admin.conf /home/$username/.kube/config
-    chown $username:$username /home/$username/.kube/config
+    cp -i /etc/kubernetes/admin.conf /home/$1/.kube/config
+    chown $1 /home/$1/.kube/config
 
-    runuser -l $username -c "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+    runuser -l $username -c "kubectl apply -f https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 else
     echo "Err: Missing username command line argument."
